4a6622ec1caeb0a106d415d9fd6919d7c7edb6a2
unknown <kerollensoares@WKSCPD11.consulplan.br>
2013-04-24 09:24:31 -0300

    Criacao do projeto estagios

---
+++ andia-agency/assets/functions/utils.php
@@ -0,0 +1,41 @@
+<? 
+
+function link_to($descricao, $url) {
+
+        return "<a href=\"$url\">$descricao</a>";
+        
+}
+
+function render_partial($pagina) {
+    
+        if(file_exists("pags/$pagina.php")) {
+                include("pags/$pagina.php");    
+        } else {
+                include("pags/home.php");       
+        }
+    
+}
+
+function validaCaracter($str) {
+        
+        if($str != '') return "'" . strtoupper($str) . "'"; else return "NULL";
+        
+}
+
+function retornaTelefone(){
+    $sql = "SELECT TELEFONES FROM FILIAL WHERE IDFILIAL = '01'";
+    $result = ibase_query($sql) or die(ibase_errmsg());
+    $row = ibase_fetch_object($result);
+    return $telefone = $row->TELEFONES;
+}
+
+function retornaEndereco(){
+        $sql ="SELECT ENDERECO FROM FILIAL WHERE IDFILIAL = '01'";
+        $result = ibase_query($sql) or die(ibase_errmsg());
+    $row = ibase_fetch_object($result);
+    return $endereco = $row->ENDERECO;
+}
+
+
+
+?>
